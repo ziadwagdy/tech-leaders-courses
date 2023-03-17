d5e6c0d6c5aa4d2395ad205413f1e2a9829398b6
ziadwagdy <zwagdy@gmail.com>
2023-03-17 12:09:31 +0300

    session-2-php

---
+++ Backend/type.php
@@ -0,0 +1,15 @@
+<?php
+    $var = "100$"; // string => int
+    $var1 = "ABC"; // string => int | float | real
+
+    $var = (int) $var;
+    echo $var;
+    echo '</br>';
+
+    // no casting : return 0
+    $var1 = (int) $var1;
+    echo $var1;
+
+    // Type Juggling
+    echo '</br>';
+//    echo 12 + '12$'; // error
